16728ba439df3b3132cc44e47f72597f634da15f
Navya Akkiraju <Navya@Navyas-MacBook-Air.local>
2020-08-23 21:32:10 -0400

    Show all snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -26,7 +26,8 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project',default=None,help="Only snapshots for project (tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all','list_all',default=False,is_flag=True,help="list all snapshots for each volume, not just latest snapshot")
+def list_snapshots(project,list_all):
     "list EC2 snapshots"
     instances=filter_instances(project)
 
@@ -41,6 +42,8 @@ def list_snapshots(project):
                 s.progress,
                 s.start_time.strftime("%c")
                 )))
+
+                if s.state=='completed' and not list_all:break
 
     return
 
