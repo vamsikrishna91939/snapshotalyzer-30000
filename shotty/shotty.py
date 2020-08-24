431f43713d1809dcc831dd87b54b9223d4901093
Navya Akkiraju <Navya@Navyas-MacBook-Air.local>
2020-08-23 21:22:21 -0400

    Error handling while starting and stopping instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import botocore
 
 session=boto3.Session(profile_name='shotty')
 ec2=session.resource('ec2')
@@ -114,7 +115,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}".format(i.id)+str(e))
+            continue
     return
 
 @instances.command('start')
@@ -125,7 +130,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}".format(i.id)+str(e))
+            continue
     return
 
 if __name__=='__main__':
